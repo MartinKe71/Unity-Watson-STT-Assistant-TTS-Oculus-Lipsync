ad579874d5265cf6b13cefcf5069af8e2bf7f605
MartinKe71 <tianrunk@andrew.cmu.edu>
2021-09-22 16:26:27 -0400

    Update VoiceFlowTest.cs

--- Assets/Scripts/VoiceFlowTest.cs
+++ Assets/Scripts/VoiceFlowTest.cs
@@ -19,7 +19,7 @@ public class VoiceFlowTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ProcessChat("");
     }
 
     // Update is called once per frame
